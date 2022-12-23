94398d69d970e7669df6fe2d83fce969726d398f
Ramai Alejandria <vyasadev931@gmail.com>
2022-12-23 22:49:52 +0100

    After Finally

---
+++ AfterFinally/src/com/luv2code/aopdemo/afterfinally/DemoConfig.java
@@ -0,0 +1,13 @@
+package com.luv2code.aopdemo.afterfinally;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.luv2code.aopdemo.afterfinally")
+public class DemoConfig {
+
+
+}
